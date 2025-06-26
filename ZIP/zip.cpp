277ce3b9bebf95fdb85400a1450bad51d4388517
YSerhanGENER <gener@email.arizona.edu>
2025-06-26 16:34:07 -0700

    Updated ZIP kernel without size limitation and 3 instance ZIP creation config

--- ZIP/zip.cpp
+++ ZIP/zip.cpp
@@ -1,14 +1,15 @@
 #include <hls_stream.h>
 #include <ap_int.h>
 
-// #define DATA_WIDTH 32
+#define BUFFER_SIZE 4096
 
 // typedef ap_int<DATA_WIDTH> data_t;
 
 typedef float data_t;
 
-enum Operation { ADD = 0, SUB = 1, MULT = 2, DIV = 3 };
+enum Operation { ADD = 0, SUB = 1, MULT = 2, DIV = 3 , MULT_RE = 4};
 
+extern "C" {
 void zip(
     const data_t* input1,
     const data_t* input2,
@@ -16,9 +17,9 @@ void zip(
     Operation op,
     int size
 ) {
-    #pragma HLS INTERFACE m_axi port=input1 offset=slave bundle=hbm0
-    #pragma HLS INTERFACE m_axi port=input2 offset=slave bundle=hbm1
-    #pragma HLS INTERFACE m_axi port=output offset=slave bundle=hbm2
+    #pragma HLS INTERFACE m_axi port=input1 offset=slave bundle=float_in max_read_burst_length=16 num_read_outstanding=64
+    #pragma HLS INTERFACE m_axi port=input2 offset=slave bundle=float_in max_read_burst_length=16 num_read_outstanding=64
+    #pragma HLS INTERFACE m_axi port=output offset=slave bundle=float_out max_write_burst_length=16 num_write_outstanding=64
 
     #pragma HLS INTERFACE s_axilite port=input1 bundle=control
     #pragma HLS INTERFACE s_axilite port=input2 bundle=control
@@ -27,30 +28,64 @@ void zip(
     #pragma HLS INTERFACE s_axilite port=size bundle=control
     #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-    data_t a, b, result;
-
-    for (int i = 0; i < size; i++) {
-        #pragma HLS PIPELINE II=1
-        a = input1[i];
-        b = input2[i];
-
+    data_t input1_bufffer[BUFFER_SIZE];
+    data_t input2_bufffer[BUFFER_SIZE];
+    data_t output_bufffer[BUFFER_SIZE];
+    int chunk_size;
+    for (int i = 0; i < size; i+=BUFFER_SIZE) {
+        if ((i + BUFFER_SIZE) > size){
+            chunk_size = size - i;
+        }
+        read_input1: for (int j = 0; j < chunk_size; j++) {
+            #pragma HLS PIPELINE II=1
+            input1_bufffer[j] = input1[i+j];
+        }
+        read_input2: for (int j = 0; j < chunk_size; j++) {
+            #pragma HLS PIPELINE II=1
+            input2_bufffer[j] = input2[i+j];
+        }
         switch (op) {
             case ADD:
-                result = a + b;
+                add: for (int j = 0; j < chunk_size; j++) {
+                    #pragma HLS PIPELINE II=1
+                    output_bufffer[j] = input1_bufffer[j] + input2_bufffer[j];
+                }
                 break;
             case SUB:
-                result = a - b;
+                sub: for (int j = 0; j < chunk_size; j++) {
+                    #pragma HLS PIPELINE II=1
+                    output_bufffer[j] = input1_bufffer[j] - input2_bufffer[j];
+                }
                 break;
             case MULT:
-                result = a * b;
+                mult: for (int j = 0; j < chunk_size; j+=2) {
+                    #pragma HLS PIPELINE II=1
+                    output_bufffer[j]   = input1_bufffer[j] * input2_bufffer[j]   - input1_bufffer[j+1] * input2_bufffer[j+1];
+                    output_bufffer[j+1] = input1_bufffer[j] * input2_bufffer[j+1] + input1_bufffer[j+1] * input2_bufffer[j];
+                }
                 break;
             case DIV:
-                result = (b != 0) ? (a / b) : 0;
+                div: for (int j = 0; j < chunk_size; j++) {
+                    #pragma HLS PIPELINE II=1
+                    output_bufffer[j] = (input2_bufffer[j] != 0) ? (input1_bufffer[j] / input2_bufffer[j]) : 0;
+                }
+                break;
+            case MULT_RE:
+                mult_re: for (int j = 0; j < chunk_size; j++) {
+                    #pragma HLS PIPELINE II=1
+                    output_bufffer[j] = input1_bufffer[j] * input2_bufffer[j];
+                }
                 break;
             default:
-                result = 0; // Default case for safety
+                for (int j = 0; j < chunk_size; j++) {
+                    #pragma HLS PIPELINE II=1
+                    output_bufffer[j] = 0;
+                }
         }
-
-        output[i] = result;
+        write_output: for (int j = 0; j < chunk_size; j++) {
+            #pragma HLS PIPELINE II=1
+            output[i + j] = output_bufffer[j];
+        }
     }
+}
 }
