47f03f756901caf15f5cd6888a891dbc1d907b49
YSerhanGENER <gener@email.arizona.edu>
2025-08-19 07:14:48 -0700

    Minor changes based on initial build testing and gitignore updates

--- vivado/AUP-ZU3_2024.2.tcl
+++ vivado/AUP-ZU3_2024.2.tcl
@@ -205,9 +205,9 @@ set_property "ip_repo_paths" "[file normalize "$origin_dir/../vitis_hls/IPs"]" [
 update_ip_catalog -rebuild
 
 # Source our BD TCL
-source "$origin_dir/1fft1zip_bd.tcl"
+source "$origin_dir/fft1xzip1x_bd.tcl"
 
 # Make the wrapper, add it to our fileset, and set it as the top module
-make_wrapper -files [get_files 1fft1zip.bd] -top -import
+make_wrapper -files [get_files fft1xzip1x.bd] -top -import
 
 puts "Vivado project created! Feel free to open it for synthesis and/or editing!"
--- vivado/fft1xzip1x_bd.tcl
+++ vivado/fft1xzip1x_bd.tcl
@@ -1,6 +1,6 @@
 
 ################################################################
-# This is a generated script based on design: 1fft1zip
+# This is a generated script based on design: fft1xzip1x
 #
 # Though there are limitations about the generated script,
 # the main purpose of this utility is to make learning
@@ -41,7 +41,7 @@ if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
 ################################################################
 
 # To test this script, run the following commands from Vivado Tcl console:
-# source 1fft1zip_bd.tcl
+# source fft1xzip1x_bd.tcl
 
 # If there is no project opened, this script will create a
 # project, but make sure you do not have an existing project
@@ -55,7 +55,7 @@ if { $list_projs eq "" } {
 
 # CHANGE DESIGN NAME HERE
 variable design_name
-set design_name 1fft1zip
+set design_name fft1xzip1x
 
 # If you do not already have an existing IP Integrator design open,
 # you can create a design using the following command:
