2b9e572f4ceeb484abd66a5a87ab64b9cd06afa1
suluhan <suluhanumut@gmail.com>
2025-11-24 21:04:38 -0700

    Moved data type conversions to accelerator

--- complex_gemv_float/complex_gemv_flt.cpp
+++ complex_gemv_float/complex_gemv_flt.cpp
@@ -7,10 +7,17 @@
 
 typedef float data_t;
 
+typedef float cedr_re_flt_type;
 typedef struct {
     data_t re;
     data_t im;
 } cedr_cmplx_flt_type;
+
+typedef double cedr_re_dbl_type;
+typedef struct {
+    double re;
+    double im;
+} cedr_cmplx_dbl_type;
 
 const unsigned A_ROWS = 8;
 const unsigned A_COLS = 8;
@@ -54,18 +61,42 @@ row_loop:
     }
 }
 
-void complex_gemv_flt(cedr_cmplx_flt_type *input1, cedr_cmplx_flt_type *input2, cedr_cmplx_flt_type *output){
+void complex_gemv_flt(cedr_cmplx_dbl_type *input1, cedr_cmplx_dbl_type *input2, cedr_cmplx_dbl_type *output){
 
-#pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input1 max_read_burst_length = 256
-#pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input2 max_read_burst_length = 256
-#pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=output max_read_burst_length = 256
+    #pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input1 max_read_burst_length = 256
+    #pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input2 max_read_burst_length = 256
+    #pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=output max_read_burst_length = 256
 
-#pragma HLS INTERFACE s_axilite port=input1  bundle=control
-#pragma HLS INTERFACE s_axilite port=input2  bundle=control
-#pragma HLS INTERFACE s_axilite port=output  bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
+    #pragma HLS INTERFACE s_axilite port=input1  bundle=control
+    #pragma HLS INTERFACE s_axilite port=input2  bundle=control
+    #pragma HLS INTERFACE s_axilite port=output  bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
 
+    static cedr_cmplx_flt_type input1_flt[A_ROWS * A_COLS];
+    static cedr_cmplx_flt_type input2_flt[A_COLS];
+    static cedr_cmplx_flt_type output_flt[A_ROWS];
 
-wrapper_complex_gemv_flt_hw(input1, input2, output);
+    convert_input1:
+    for (size_t i = 0; i < A_ROWS * A_COLS; i++) {
+        #pragma HLS pipeline II=1
+        input1_flt[i].re = static_cast<cedr_re_flt_type>(input1[i].re);
+        input1_flt[i].im = static_cast<cedr_re_flt_type>(input1[i].im);
+    }
+
+    convert_input2:
+    for (size_t i = 0; i < A_COLS; i++) {
+        #pragma HLS pipeline II=1
+        input2_flt[i].re = static_cast<cedr_re_flt_type>(input2[i].re);
+        input2_flt[i].im = static_cast<cedr_re_flt_type>(input2[i].im);
+    }
+
+    wrapper_complex_gemv_flt_hw(input1_flt, input2_flt, output_flt);
+
+    convert_output:
+    for (size_t i = 0; i < A_ROWS; i++) {
+        #pragma HLS pipeline II=1
+        output[i].re = static_cast<cedr_re_dbl_type>(output_flt[i].re);
+        output[i].im = static_cast<cedr_re_dbl_type>(output_flt[i].im);
+    }
 
 }
--- complex_gemv_int/complex_gemv_int.cpp
+++ complex_gemv_int/complex_gemv_int.cpp
@@ -8,10 +8,17 @@
 typedef int data_t;
 typedef long long accum_t;  // Use 64-bit for intermediate accumulations
 
+typedef int cedr_re_int_type;
 typedef struct {
     data_t re;
     data_t im;
 } cedr_cmplx_int_type;
+
+typedef double cedr_re_dbl_type;
+typedef struct cedr_cmplx_dbl_type {
+cedr_re_dbl_type re;
+cedr_re_dbl_type im;
+} cedr_cmplx_dbl_type;
 
 // Saturation function: clamp 64-bit value to 32-bit int range
 inline data_t saturate_to_int(accum_t value) {
@@ -70,18 +77,43 @@ row_loop:
     }
 }
 
-void complex_gemv_int(cedr_cmplx_int_type *input1, cedr_cmplx_int_type *input2, cedr_cmplx_int_type *output){
+void complex_gemv_int(cedr_cmplx_dbl_type *input1, cedr_cmplx_dbl_type *input2, cedr_cmplx_dbl_type *output){
 
-#pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input1 max_read_burst_length = 256
-#pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input2 max_read_burst_length = 256
-#pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=output max_read_burst_length = 256
+    #pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input1 max_read_burst_length = 256
+    #pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=input2 max_read_burst_length = 256
+    #pragma HLS INTERFACE m_axi offset=SLAVE bundle=gmem port=output max_read_burst_length = 256
 
-#pragma HLS INTERFACE s_axilite port=input1  bundle=control
-#pragma HLS INTERFACE s_axilite port=input2  bundle=control
-#pragma HLS INTERFACE s_axilite port=output  bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
+    #pragma HLS INTERFACE s_axilite port=input1  bundle=control
+    #pragma HLS INTERFACE s_axilite port=input2  bundle=control
+    #pragma HLS INTERFACE s_axilite port=output  bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
 
+    static cedr_cmplx_int_type input1_int[A_ROWS * A_COLS];
+    static cedr_cmplx_int_type input2_int[A_COLS];
+    static cedr_cmplx_int_type output_int[A_ROWS];
 
-wrapper_complex_gemv_int_hw(input1, input2, output);
+    convert_input1:
+    for (size_t i = 0; i < A_ROWS * A_COLS; i++) {
+        #pragma HLS pipeline II=1
+        input1_int[i].re = static_cast<cedr_re_int_type>(input1[i].re);
+        input1_int[i].im = static_cast<cedr_re_int_type>(input1[i].im);
+    }
+
+    convert_input2:
+    for (size_t i = 0; i < A_COLS; i++) {
+        #pragma HLS pipeline II=1
+        input2_int[i].re = static_cast<cedr_re_int_type>(input2[i].re);
+        input2_int[i].im = static_cast<cedr_re_int_type>(input2[i].im);
+    }
+    
+
+    wrapper_complex_gemv_int_hw(input1_int, input2_int, output_int);
+
+    convert_output:
+    for (size_t i = 0; i < A_ROWS; i++) {
+        #pragma HLS pipeline II=1
+        output[i].re = static_cast<cedr_re_dbl_type>(output_int[i].re);
+        output[i].im = static_cast<cedr_re_dbl_type>(output_int[i].im);
+    }
 
 }
